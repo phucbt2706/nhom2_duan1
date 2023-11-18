a688452ab1c48174b5e65d9615a19909c6d766a7
phucbt2706 <trongphuceazy06@gmail.com>
2023-11-18 23:09:50 +0700

    Update pagging.php

--- lib/pagging/pagging.php
+++ lib/pagging/pagging.php
@@ -2,16 +2,12 @@
     function get_pagging($num_page,$page){
         $str_html = '<nav aria-label="Page navigation example">
         <ul class="pagination">';
-        //Nếu $page >1 thì mới hiển thị button PREVIOUS
         if ($page>1) {
             $page_pre = $page -1;
             $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_products&page='.$page_pre.'"><<</a></li>';
         }
 
-        //Show all tất cả số trang hiện có
         for ($i=1; $i <=$num_page; $i++) { 
-            //Nếu đang ở số trang hiện tại thì bg blue
-            //Thêm class = active để kích hoạt css trong boostrap
             $active = '';
             if ($i == $page ) {
                 $active = 'active';
@@ -19,7 +15,6 @@
             $str_html .= '<li class="page-item '.$active.'"><a class="page-link" href="?pages=list_products&page='.$i.'">'.$i.'</a></li>';
         }
 
-        //Nếu $page < $num_page thì mới hiển thị button NEXt
         if ($page<$num_page) {
             $page_next = $page + 1;
             $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_products&page='.$page_next.'">>></a></li>';
@@ -33,16 +28,12 @@
     function get_pagging_cate($num_page,$page){
         $str_html = '<nav aria-label="Page navigation example">
         <ul class="pagination">';
-        //Nếu $page >1 thì mới hiển thị button PREVIOUS
         if ($page>1) {
             $page_pre = $page -1;
             $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_cate&page='.$page_pre.'"><<</a></li>';
         }
 
-        //Show all tất cả số trang hiện có
         for ($i=1; $i <=$num_page; $i++) { 
-            //Nếu đang ở số trang hiện tại thì bg blue
-            //Thêm class = active để kích hoạt css trong boostrap
             $active = '';
             if ($i == $page ) {
                 $active = 'active';
@@ -50,7 +41,6 @@
             $str_html .= '<li class="page-item '.$active.'"><a class="page-link" href="?pages=list_cate&page='.$i.'">'.$i.'</a></li>';
         }
 
-        //Nếu $page < $num_page thì mới hiển thị button NEXt
         if ($page<$num_page) {
             $page_next = $page + 1;
             $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_cate&page='.$page_next.'">>></a></li>';
@@ -58,5 +48,31 @@
         $str_html .= '</ul>
         </nav>';
         return $str_html;
+    }
+
+    function get_pagging_user($num_page,$page){
+        $str_html = '<nav aria-label="Page navigation example">
+        <ul class="pagination">';
+        if ($page>1) {
+            $page_pre = $page -1;
+            $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_user&page='.$page_pre.'"><<</a></li>';
+        }
+
+        for ($i=1; $i <=$num_page; $i++) { 
+            $active = '';
+            if ($i == $page ) {
+                $active = 'active';
+            }
+            $str_html .= '<li class="page-item '.$active.'"><a class="page-link" href="?pages=list_user&page='.$i.'">'.$i.'</a></li>';
+        }
+
+        if ($page<$num_page) {
+            $page_next = $page + 1;
+            $str_html .= '<li class="page-item"><a class="page-link" href="?pages=list_user&page='.$page_next.'">>></a></li>';
+        }
+        $str_html .= '</ul>
+        </nav>';
+        return $str_html;
+
     }
 ?>
