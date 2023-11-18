2a3995b5741c0e579b72c565d6c730bd7c738e43
nguyennhattruong-cmyk <149385224+nguyennhattruong-cmyk@users.noreply.github.com>
2023-11-18 22:04:58 +0700

    Update khach-hang.php

--- dao/khach-hang.php
+++ dao/khach-hang.php
@@ -9,55 +9,64 @@ class KhachHang extends Connect
     }
 
     // Update category
-    function user_update($username, $password, $fullname, $email, $phone, $avatar, $role_id, $user_id)
-    {
-        $sql = "UPDATE `user` SET username='$username', password='$password', fullname='$fullname', email='$email', phone='$phone', avatar='$avatar', role_id='$role_id' WHERE user_id=?";
-        $this->pdo_execute($sql, $user_id);
-    }
+    function user_update($user_id, $username, $password, $fullname, $email, $phone, $avatar, $role_id)
+{
+    $sql = "UPDATE `user` SET username=?, password=?, fullname=?, email=?, phone=?, avatar=?, role_id=? WHERE user_id=?";
+    $this->pdo_execute($sql, $username, $password, $fullname, $email, $phone, $avatar, $role_id, $user_id);
+}
+
 
     // Delete category
-    function khach_hang_delete($ma_kh)
+    function user_delete($user_id)
     {
-        $sql = "DELETE FROM khach_hang WHERE ma_kh=?";
-        if (is_array($ma_kh)) {
-            foreach ($ma_kh as $ma) {
-                $this->pdo_execute($sql, $ma);
+        $sql = "DELETE FROM user WHERE user_id=?";
+        if (is_array($user_id)) {
+            foreach ($user_id as $id) {
+                $this->pdo_execute($sql, $id);
             }
         } else {
-            $this->pdo_execute($sql, $ma_kh);
+            $this->pdo_execute($sql, $user_id);
         }
     }
 
 
     // Get all data
-    function khach_hang_select_all()
+    function user_select_all()
     {
-        $sql = "SELECT * FROM khach_hang";
+        $sql = "SELECT * FROM user";
         return  $this->pdo_query($sql);
     }
-
-    function khach_hang_select_by_id($ma_kh)
-    {
-        $sql = "SELECT * FROM khach_hang WHERE ma_kh=?";
-        return  $this->pdo_query_one($sql, $ma_kh);
+    function num_row_user(){
+        $sql = "SELECT count(*) as num_row FROM user";
+        return $this->pdo_query($sql);
+    }
+    function user_select_page($start,$num_rows_in_page){
+        $sql = "SELECT * FROM user LIMIT $start,$num_rows_in_page";
+        return $this->pdo_query($sql);
     }
 
-    function khach_hang_exist($ma_kh)
+    function user_select_by_id($user_id)
     {
-        $sql = "SELECT count(*) FROM khach_hang WHERE ma_kh=?";
-        return  $this->pdo_query_value($sql, $ma_kh) > 0;
+        $sql = "SELECT * FROM user WHERE user_id=?";
+        return  $this->pdo_query_one($sql, $user_id);
     }
 
-    function khach_hang_select_by_role($vai_tro)
+    function user_exist($user_id)
     {
-        $sql = "SELECT * FROM khach_hang WHERE vai_tro=?";
+        $sql = "SELECT count(*) FROM user WHERE user_id=?";
+        return  $this->pdo_query_value($sql, $user_id) > 0;
+    }
+
+    function user_select_by_role($vai_tro)
+    {
+        $sql = "SELECT * FROM user WHERE vai_tro=?";
         return  $this->pdo_query_one($sql, $vai_tro);
     }
 
-    function khach_hang_change_password($ma_kh, $mat_khau_moi)
+    function user_change_password($user_id, $mat_khau_moi)
     {
-        $sql = "UPDATE khach_hang SET mat_khau=? WHERE ma_kh=?";
-        $this->pdo_execute($sql, $mat_khau_moi, $ma_kh);
+        $sql = "UPDATE user SET mat_khau=? WHERE user_id=?";
+        $this->pdo_execute($sql, $mat_khau_moi, $user_id);
     }
 
     function kiemTraSoDienThoai($soDienThoai)
