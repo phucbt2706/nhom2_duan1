08ccab5b89f12ad8cf7a619a9e5ffe59895e5fbd
nguyennhattruong-cmyk <149385224+nguyennhattruong-cmyk@users.noreply.github.com>
2023-11-18 22:05:31 +0700

    Update list.php

--- admin/resource/roles/list.php
+++ admin/resource/roles/list.php
@@ -3,48 +3,44 @@
         <div class="card">
             <h5 class="card-header">Danh sách vai trò</h5>
             <div class="table-responsive text-nowrap">
-                <form action="?btn_deleteall" method="post">
+                <form action="?pages=delete_all_role" method="post">
                     <button type="submit" class="btn btn-secondary" name="delete_all">Delete</button>
-                    <a class="btn btn-primary" role="button" href="index.php">Add</a>
+                    <a class="btn btn-primary" role="button" href="?pages=add_role">Add</a>
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th scope="col">Tên đăng nhập</th>
-                                <th scope="col">Mật khẩu</th>
-                                <th scope="col">Họ và tên</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Hình</th>
-                                <th scope="col">Trạng thái</th>
-                                <th scope="col">Vai trò</th>
-                                <th scope="col"></th>
+                            <th><input type="checkbox" name="role_id[]" id="role1" onclick="checkedAllRoles();"></th>
+                                <th>ID.</th>                             
+                                <th>Role name</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
 
                         <tbody>
+                        <?php
+                                $stt = 0;
+                                foreach ($list_role as $item) {
+                                extract($item);
+                                $stt++;?>
                             <tr>
-                                <td> <input type="checkbox" class="name1" name="kh[]" value=""></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                            <td> <input type="checkbox" class="role" name="role_id[]" value="<?=$role_id?>"></td>
+                                <td><?= $role_id ?></td>
+                                <td><?= $role_name ?></td>
                                 <td>
                                     <div class="dropdown">
-                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                            data-bs-toggle="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
                                             <i class="bx bx-dots-vertical-rounded"></i>
                                         </button>
                                         <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="index.php?btn_edit&ma_kh="><i
-                                                    class="bx bx-edit-alt me-1"></i>Edit</a>
-                                            <a class="dropdown-item" href="index.php?btn_delete&ma_kh="><i
-                                                    class="bx bx-edit-alt me-1"></i>Delete</a>
+                                            <a class="dropdown-item" href="?pages=edit_role&role_id=<?= $role_id ?>"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                                            <a class="dropdown-item" href="?pages=delete_role&role_id=<?= $role_id ?>"><i class="bx bx-edit-alt me-1"></i> Delete</a>
                                         </div>
                                     </div>
                                 </td>
                             </tr>
+                            <?php
+                                    }
+                                    ?>
 
                         </tbody>
                     </table>
