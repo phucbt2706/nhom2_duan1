b87ae6715bdcbf2b8723074cde0b812cf0b7f2b6
nguyennhattruong-cmyk <149385224+nguyennhattruong-cmyk@users.noreply.github.com>
2023-11-18 22:06:08 +0700

    Update form-add.php

--- admin/resource/roles/form-add.php
+++ admin/resource/roles/form-add.php
@@ -2,81 +2,25 @@
     <div class="col-7">
         <div class="card mb-4">
             <div class="card-body">
-                <form action="?pages=add_role" method="post" id="myForm" enctype="multipart/form-data">
+                <form action="?pages=insert_role" method="post" id="myForm" enctype="multipart/form-data">
                     <div class="row mb-4">
                         <div class="col-12 text-center">
-                            <h2>Thêm người dùng</h2>
+                            <h2>Thêm Vai Trò</h2>
                         </div>
-                        <div class="col-6">
-                            <label for="" class="form-label required">Tên đăng nhập</label>
-                            <input type="" class="form-control" id="" value="">
-                                    
+                        <div class="col-6 form-group">
+                            <label for="" class="form-label required">Role Name</label>
+                            <input type="" class="form-control" id="" value="<?php echo (!empty($_POST['role_name'])) ? $_POST['role_name'] : false; ?>" name="role_name">
+                            <span id="red_error" class="form-text"><?php if (!empty($error['name_format'])) {
+                                                            echo $error['name_format'];
+                                                        } else if (!empty($error['name_empty'])) {
+                                                            echo $error['name_empty'];
+                                                        }
+                                                        ?> </span>
                         </div>
-                        <div class=" col-6">
-                            <label for="" class="form-label required">Tên khách hàng</label>
-                            <input type="" class="form-control" id=" " value="" name="ten_kh">
-
-                        </div>
-                        <div class="col-6">
-                            <label for="inputPassword4" class="form-label required">Mật khẩu</label>
-                            <input type="password" class="form-control" id="inputPassword4" value="">
-
-                        </div>
-                        <div class="col-6">
-                            <label for="inputPassword4" class="form-label required">Xác nhận mật khẩu</label>
-                            <input type="password" class="form-control" id="inputPassword4"
-                                value=""
-                                name="mat_khau2">
-
-                        </div>
-                        <div class="col-6">
-                            <label for="exampleFormControlInput1" class="form-label required">Email</label>
-                            <input type="email" name="email" class="form-control" value=""
-                                placeholder="name@example.com">
-
-                        </div>
-                        <div class="col-6">
-                            <label for="formFile" class="form-label required">Chọn hình ảnh</label>
-                            <input class="form-control" name="hinh" type="file" id="formFile">
-                        </div>
-                        <div class="col-6">
-                            <label for="" class="form-label required">Trạng thái</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="kich_hoat" value="0"
-                                    id="status-radio1">
-                                <label class="form-check-label" for="status-radio1">
-                                    Chưa kích hoạt
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="kich_hoat" value="1"
-                                    id="status-radio2" checked>
-                                <label class="form-check-label" for="status-radio2">
-                                    Kích hoạt
-                                </label>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <label for="" class="form-label required">Vai trò</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="vai_tro" value="0" id="role-radio3">
-                                <label class="form-check-label" for="role-radio3">
-                                    Khách hàng
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="vai_tro" value="1" id="role-radio4"
-                                    checked>
-                                <label class="form-check-label" for="role-radio">
-                                    Nhân viên
-                                </label>
-                            </div>
                         </div>
                         <div class="col-12 mt-4">
-                            <button type="submit" name="btn_insert" value="btn_insert" class="btn btn-primary">Thêm
-                                mới</button>
-                            <button type="submit" name="btn_list" value="btn_list" class="btn btn-primary">Danh
-                                sách</button>
+                        <button type="submit" class="btn btn-primary">Add</button>
+                            <a class="btn btn-primary" href="?pages=list_role" role="button">List of Roles</a>
                         </div>
                     </div>
                 </form>
