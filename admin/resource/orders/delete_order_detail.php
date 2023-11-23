b62a8d4e19a46a2608657f3f9085e3ee222cdc88
phucbt2706 <trongphuceazy06@gmail.com>
2023-11-23 21:51:51 +0700

    Update delete_order_detail.php

--- admin/resource/orders/delete_order_detail.php
+++ admin/resource/orders/delete_order_detail.php
@@ -6,7 +6,7 @@ $order_id = $_GET['order_id'];
 try {
     $order->order_detail_delete($product_id,$order_id );
     echo "<script>alert(\"Delete product successfully! \");</script>";
-    echo "<script>window.location.href ='?pages=orders';</script>";
+    echo "<script>window.location.href ='?pages=order_detail&order_id={$order_id}';</script>";
 } catch (PDOException $e) {
     throw $e;
 }
