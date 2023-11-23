7b0c88aa87a9cdd0559c847e3a1a579a85f87819
phucbt2706 <trongphuceazy06@gmail.com>
2023-11-23 10:45:33 +0700

    Update list.php

--- admin/resource/categories/list.php
+++ admin/resource/categories/list.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-12">
         <div class="card">
             <div class="text-center">
                 <h3 class="card-header">Categories list</h3>
