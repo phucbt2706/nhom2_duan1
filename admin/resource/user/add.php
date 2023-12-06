4d32e2bcd4d0c065b9f9905b16dc28743703fdd0
nguyennhattruong-cmyk <149385224+nguyennhattruong-cmyk@users.noreply.github.com>
2023-12-06 18:39:56 +0700

    Update add.php

--- admin/resource/user/add.php
+++ admin/resource/user/add.php
@@ -9,28 +9,54 @@
                         </div>
                         <div class="col-6">
                             <label for="" class="form-label required">Username</label>
-                            <input type="" class="form-control <?= !empty($error['error_username']) ? 'border border-danger' : false ?> " id=" " value="<?= !empty($_POST['username']) ? $_POST['username'] : false ?>" name="username">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_username'])?$error['error_username']:false?> </span>
+                            <input type=""
+                                class="form-control <?= !empty($error['error_username']) ? 'border border-danger' : false ?> "
+                                id=" " value="<?= !empty($_POST['username']) ? $_POST['username'] : false ?>"
+                                name="username">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_username']) ? $error['error_username'] : false ?>
+                            </span>
                         </div>
                         <div class="col-6">
                             <label for="" class="form-label required">Fullname</label>
-                            <input type="" class="form-control <?= !empty($error['error_name']) ? 'border border-danger' : false ?>" id=" " value="<?= !empty($_POST['fullname']) ? $_POST['fullname'] : false ?>" name="fullname">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_name'])?$error['error_name']:false;?> </span>
+                            <input type=""
+                                class="form-control <?= !empty($error['error_name']) ? 'border border-danger' : false ?>"
+                                id=" " value="<?= !empty($_POST['fullname']) ? $_POST['fullname'] : false ?>"
+                                name="fullname">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_name']) ? $error['error_name'] : false; ?>
+                            </span>
                         </div>
                         <div class="col-6">
                             <label for="inputPassword4" class="form-label required">Password</label>
-                            <input type="password" class="form-control <?= !empty($error['error_pass']) ? 'border border-danger' : false ?>" id="" name="password" value="<?= !empty($_POST['password']) ? $_POST['password'] : false ?>">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_pass'])?$error['error_pass']:false;?> </span>
+                            <input type="password"
+                                class="form-control <?= !empty($error['error_pass']) ? 'border border-danger' : false ?>"
+                                id="" name="password"
+                                value="<?= !empty($_POST['password']) ? $_POST['password'] : false ?>">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_pass']) ? $error['error_pass'] : false; ?>
+                            </span>
                         </div>
                         <div class="col-6">
                             <label for="inputPassword4" class="form-label required">Confirm Password</label>
-                            <input type="password" class="form-control <?= !empty($error['error_conf_pass']) ? 'border border-danger' : false ?>" id="" value="<?= !empty($_POST['confirm_password']) ? $_POST['confirm_password'] : false ?>" name="confirm_password">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_conf_pass'])?$error['error_conf_pass']:false;?> </span>
+                            <input type="password"
+                                class="form-control <?= !empty($error['error_conf_pass']) ? 'border border-danger' : false ?>"
+                                id=""
+                                value="<?= !empty($_POST['confirm_password']) ? $_POST['confirm_password'] : false ?>"
+                                name="confirm_password">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_conf_pass']) ? $error['error_conf_pass'] : false; ?>
+                            </span>
                         </div>
                         <div class="col-6">
                             <label for="exampleFormControlInput1" class="form-label required">Email</label>
-                            <input type="email" name="email" class="form-control <?= !empty($error['error_email']) ? 'border border-danger' : false ?>" value="<?= !empty($_POST['email']) ? $_POST['email'] : false ?>" placeholder="name@example.com">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_email'])?$error['error_email']:false;?> </span>
+                            <input type="email" name="email"
+                                class="form-control <?= !empty($error['error_email']) ? 'border border-danger' : false ?>"
+                                value="<?= !empty($_POST['email']) ? $_POST['email'] : false ?>"
+                                placeholder="name@example.com">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_email']) ? $error['error_email'] : false; ?>
+                            </span>
                         </div>
                         <div class="col-6">
                             <label for="formFile" class="form-label required">Avatar</label>
@@ -38,9 +64,23 @@
                         </div>
                         <div class="col-6">
                             <label for="" class="form-label required">Phone</label>
-                            <input type="text" name="phone" class="form-control <?= !empty($error['error_phone']) ? 'border border-danger' : false ?>" value="<?= !empty($_POST['phone']) ? $_POST['phone'] : false ?>">
-                            <span id="red_error" class="form-text"><?= !empty($error['error_phone'])?$error['error_phone']:false;?> </span>
+                            <input type="text" name="phone"
+                                class="form-control <?= !empty($error['error_phone']) ? 'border border-danger' : false ?>"
+                                value="<?= !empty($_POST['phone']) ? $_POST['phone'] : false ?>">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_phone']) ? $error['error_phone'] : false; ?>
+                            </span>
                         </div>
+                        <div class="col-6">
+                            <label for="" class="form-label required">Address</label>
+                            <input type="text"
+                                class="form-control <?= !empty($error['error_address']) ? 'border border-danger' : '' ?>"
+                                id="" value="<?= !empty($_POST['address']) ? $_POST['address'] : '' ?>" name="address">
+                            <span id="red_error" class="form-text">
+                                <?= !empty($error['error_address']) ? $error['error_address'] : '' ?>
+                            </span>
+                        </div>
+
                         <div class="col-6">
                             <label for="" class="form-label required">Role</label>
                             <div class="form-check">
@@ -50,7 +90,8 @@
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="role_id" value="1" id="role-radio4" checked>
+                                <input class="form-check-input" type="radio" name="role_id" value="1" id="role-radio4"
+                                    checked>
                                 <label class="form-check-label" for="role-radio">
                                     Admin
                                 </label>
