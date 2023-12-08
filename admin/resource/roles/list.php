9ea87b30c557bd2c8f42255a74996355603b7f19
nguyennhattruong-cmyk <149385224+nguyennhattruong-cmyk@users.noreply.github.com>
2023-12-08 19:25:05 +0700

    Update list.php

--- admin/resource/roles/list.php
+++ admin/resource/roles/list.php
@@ -6,7 +6,8 @@
             </div>
             <div class="table-responsive text-nowrap">
                 <form action="?pages=delete_all_role" method="post">
-                    <button type="submit" class="btn btn-secondary" name="delete_all">Delete</button>
+                <button type="submit" class="btn btn-secondary" name="delete_all"
+                        onclick="showDelete_all_Confirmation()">Delete</button>
                     <a class="btn btn-primary" role="button" href="?pages=add_role">Add</a>
                     <table class="table table-striped">
                         <thead>
@@ -34,7 +35,10 @@
                                         </button>
                                         <div class="dropdown-menu">
                                             <a class="dropdown-item" href="?pages=edit_role&role_id=<?= $role_id ?>"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                                            <a class="dropdown-item" href="?pages=delete_role&role_id=<?= $role_id ?>"><i class="bx bx-edit-alt me-1"></i> Delete</a>
+                                            <a class="dropdown-item" href="javascript:void(0);"
+                                                    onclick="showConfirmation(<?= $role_id ?>)">
+                                                    <i class="bx bx-edit-alt me-1"></i>Delete
+                                                </a>
                                         </div>
                                     </div>
                                 </td>
@@ -52,3 +56,33 @@
 
     </div>
 </div>
+
+<!-- JavaScript -->
+<script>
+    function showConfirmation(role_id) {
+        // Hiển thị popup xác nhận
+        var confirmation = confirm("Bạn có chắc chắn muốn xóa Role này?");
+
+        // Kiểm tra nếu người dùng đã nhấp vào OK (đồng ý)
+        if (confirmation) {
+
+            // Chuyển hướng đến trang xóa với user_id
+            window.location.href = "?pages=delete_role&role_id=" + role_id;
+
+        }
+    }
+
+
+    // Hiển thị hộp thoại xác nhận khi click vào nút "Delete"
+    function showDelete_all_Confirmation() {
+        // Hiển thị popup xác nhận
+        var isConfirmed = confirm('Bạn có chắc chắn muốn xóa không?');
+
+        // Nếu người dùng chọn OK, thực hiện xóa
+        if (isConfirmed) {
+            // Chuyển hướng đến trang xóa với user_id
+            window.location.href = "?pages=delete_all_role&role_id=" + role_id;
+        }
+    }
+
+</script>
